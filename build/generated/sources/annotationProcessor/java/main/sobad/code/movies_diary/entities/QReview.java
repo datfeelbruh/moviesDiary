e15057f411cb60980eda3226a48235bfa8b51abc
datfeelbruh <datfeelman@gmail.com>
2023-07-15 10:14:24 +0300

    добавил классы для новой логики работы

---
+++ build/generated/sources/annotationProcessor/java/main/sobad/code/movies_diary/entities/QReview.java
@@ -0,0 +1,61 @@
+package sobad.code.movies_diary.entities;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+import com.querydsl.core.types.dsl.PathInits;
+
+
+/**
+ * QReview is a Querydsl query type for Review
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QReview extends EntityPathBase<Review> {
+
+    private static final long serialVersionUID = -1686622726L;
+
+    private static final PathInits INITS = PathInits.DIRECT2;
+
+    public static final QReview review1 = new QReview("review1");
+
+    public final QBaseModel _super = new QBaseModel(this);
+
+    //inherited
+    public final NumberPath<Long> id = _super.id;
+
+    public final QMovie movie;
+
+    public final NumberPath<Double> rating = createNumber("rating", Double.class);
+
+    public final StringPath review = createString("review");
+
+    public final QUser user;
+
+    public QReview(String variable) {
+        this(Review.class, forVariable(variable), INITS);
+    }
+
+    public QReview(Path<? extends Review> path) {
+        this(path.getType(), path.getMetadata(), PathInits.getFor(path.getMetadata(), INITS));
+    }
+
+    public QReview(PathMetadata metadata) {
+        this(metadata, PathInits.getFor(metadata, INITS));
+    }
+
+    public QReview(PathMetadata metadata, PathInits inits) {
+        this(Review.class, metadata, inits);
+    }
+
+    public QReview(Class<? extends Review> type, PathMetadata metadata, PathInits inits) {
+        super(type, metadata, inits);
+        this.movie = inits.isInitialized("movie") ? new QMovie(forProperty("movie")) : null;
+        this.user = inits.isInitialized("user") ? new QUser(forProperty("user")) : null;
+    }
+
+}
+
