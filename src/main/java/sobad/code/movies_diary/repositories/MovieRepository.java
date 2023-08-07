47228f5295d99267c7655474e73afde39f2dcf04
KhoroshkoDmitriy <48517947+datfeelbruh@users.noreply.github.com>
2023-08-07 17:14:02 +0300

    movieTitles adjustments return additional id and title

--- src/main/java/sobad/code/movies_diary/repositories/MovieRepository.java
+++ src/main/java/sobad/code/movies_diary/repositories/MovieRepository.java
@@ -7,12 +7,11 @@ import org.springframework.stereotype.Repository;
 import sobad.code.movies_diary.entities.Movie;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @Repository
 public interface MovieRepository extends CrudRepository<Movie, Long>, QuerydslPredicateExecutor<Movie> {
-    @Query("SELECT m.title FROM Movie m ORDER BY m.title LIMIT 20")
-    List<String> findMoviesName();
     Optional<Movie> findById(Long id);
     List<Movie> findAll();
 }
