cfb88a93c1fe469a317d258e75031ab51199b1d5
KhoroshkoDmitriy <48517947+datfeelbruh@users.noreply.github.com>
2023-08-23 12:55:12 +0300

    hibernate search indexer reworked

--- src/main/java/sobad/code/moviesdiary/hibernateSearch/Indexer.java
+++ src/main/java/sobad/code/moviesdiary/hibernateSearch/Indexer.java
@@ -4,16 +4,12 @@ import jakarta.persistence.EntityManager;
 import org.hibernate.search.mapper.orm.Search;
 import org.hibernate.search.mapper.orm.massindexing.MassIndexer;
 import org.hibernate.search.mapper.orm.session.SearchSession;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Indexed;
 import org.springframework.transaction.annotation.Transactional;
-import sobad.code.moviesdiary.entities.Movie;
 
 @Transactional
 @Component
-public class Indexer implements ApplicationListener<ApplicationReadyEvent> {
+public class Indexer {
     private EntityManager entityManager;
 
     private static final int THREAD_NUMBER = 4;
@@ -22,12 +18,10 @@ public class Indexer implements ApplicationListener<ApplicationReadyEvent> {
         this.entityManager = entityManager;
     }
 
-
-    @Override
-    public void onApplicationEvent(ApplicationReadyEvent event) {
+    public void indexData(String indexClassName) {
         try {
             SearchSession searchSession = Search.session(entityManager);
-            Class<?> classToIndex = Movie.class;
+            Class<?> classToIndex = Class.forName(indexClassName);
             MassIndexer massIndexer = searchSession
                     .massIndexer(classToIndex)
                     .threadsToLoadObjects(THREAD_NUMBER);
@@ -35,6 +29,8 @@ public class Indexer implements ApplicationListener<ApplicationReadyEvent> {
             massIndexer.startAndWait();
         } catch (InterruptedException e) {
             throw new RuntimeException("Indexing interrupted");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
         }
     }
 }
